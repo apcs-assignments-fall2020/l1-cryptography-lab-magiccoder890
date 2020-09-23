9e89fe34db5e8f5828d20d14d102dc205571243d
magiccoder890 <23fzhu@athenian.org>
2020-09-23 14:31:31 +0000

    I can run it properly but cannot pass the test:(

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,49 +1,105 @@
 import java.util.Scanner;
 
 public class Caesar {
-    public static String encryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+    public static String encryptCaesar(String message){
+        String str = "";
+        for (int i = 0; i < message.length(); i++) {
+            char every_word = message.charAt(i);
+            int new_char = 0;
+            if (((int) every_word <=87 && (int) every_word >= 65)|| ((int) every_word <= 119 && (int) every_word >= 97)) {
+                new_char = (int)every_word + 3;
+                str += (char)new_char;
+            }
+            else if (((int) every_word <=90 && (int) every_word >= 88)|| ((int) every_word <= 122 && (int) every_word >= 120)) {
+                new_char = (int)every_word - 23;
+                str += (char)new_char;
+            }
+            else {
+                str += every_word;
+            }
+        }
+        System.out.print(str);
+        return str;
     }
 
     public static String decryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String str = "";
+        for (int i = 0; i < message.length(); i++) {
+            char every_word = message.charAt(i);
+            int new_char = 0;
+            if (((int) every_word <= 90 && (int) every_word > 67) || ((int) every_word <= 122 && (int) every_word > 99)) {
+                new_char = (int) every_word - 3;
+                str += (char) new_char;
+            } else if (((int) every_word <= 67 && (int) every_word >= 65) || ((int) every_word <= 99 && (int) every_word >= 97)) {
+                new_char = (int) every_word + 23;
+                str += (char) new_char;
+            } else {
+                str += every_word;
+            }
+        }
+        System.out.print(str);
+        return str;
     }
 
     public static String encryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String str = "";
+        for (int i = 0; i < message.length(); i++) {
+            char every_word = message.charAt(i);
+            int new_char = 0;
+            if (((int) every_word <= (90-key) && (int) every_word >= 65)|| ((int) every_word <= (122-key) && (int) every_word >= 97)) {
+                new_char = (int)every_word + key;
+                str += (char)new_char;
+            }
+            else if (((int) every_word < 90 && (int) every_word >= (91-key))|| ((int) every_word < 122 && (int) every_word >= (123-key))) {
+                new_char = (int)every_word - (26-key);
+                str += (char)new_char;
+            }
+            else {
+                str += every_word;
+            }
+        }
+        System.out.print(str);
+        return str;
     }
 
     public static String decryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String str = "";
+        for (int i = 0; i < message.length(); i++) {
+            char every_word = message.charAt(i);
+            int new_char = 0;
+            if (((int) every_word <= 90 && (int) every_word > (64 + key)) || ((int) every_word <= 122 && (int) every_word > (96 + key))) {
+                new_char = (int) every_word - key;
+                str += (char) new_char;
+            } else if (((int) every_word <= (64 + key) && (int) every_word >= 65) || ((int) every_word <= (96 + key) && (int) every_word >= 97)) {
+                new_char = (int) every_word + (26 - key);
+                str += (char) new_char;
+            } else {
+                str += every_word;
+            }
+        }
+        System.out.print(str);
+        return str;
     }
 
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Would you like to \"encrypt\" or \"decrypt\" a message?");
-        String command = scan.nextLine().trim().toLowerCase();
-
-        if (command.equals("encrypt")) {
-            System.out.println("Please enter your message to be encrypted: ");
-            String message = scan.nextLine();
-            System.out.println("Here is your encrypted message: ");
-            System.out.println(encryptCaesar(message));
+    public static void main (String[]arg){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Would you like to \"encrypt\" or \"decrypt\" a message");
+            String choice = scanner.nextLine();
+            if (choice.equals("encrypt")) {
+                System.out.print("Please enter your message to be encrypted:");
+                String message = scanner.nextLine();
+                System.out.print("Please tell out how many key you want to space");
+                int key = scanner.nextInt() % 26;
+                key %= 26;
+                encryptCaesarKey(message, key);
+            }
+            else {
+                System.out.print("Please inpnt a sentence that you want to be decrypted");
+                String message = scanner.nextLine();
+                System.out.print("Please tell out how many key you want to space");
+                int key = scanner.nextInt();
+                key %= 26;
+                decryptCaesarKey(message, key);
+            }
         }
-        else if (command.equals("decrypt")) {
-            System.out.println("Please enter your message to be decrypted: ");
-            String message = scan.nextLine();
-            System.out.println("Here is your decrypted message: ");
-            System.out.println(decryptCaesar(message));
-        }
-        else {
-            System.out.println("Unknown command; please type either \"encrypt\" or \"decrypt\"");
-        }
-
-        scan.close();
-    }
 }
